51499cebf9b7d85149355442cc4a49aa9017cae9
GenshinStore <genshingaming20@gmail.com>
2026-05-06 21:48:05 +0700

    r

--- core.js
+++ core.js
@@ -30,7 +30,7 @@ const SECONDARY_GROUP_ID = '120363426296094605@g.us';
 // const PRIMARY_GROUP_ID = '120363426296094605@g.us';
 // const SECONDARY_GROUP_ID = '120363408426078537@g.us';
 
-const ENABLE_FORWARD_TO_SECONDARY = false;
+const ENABLE_FORWARD_TO_SECONDARY = true;
 const DELAY_SECONDARY_MS = 1000; 
 const CACHE_TTL_MS = 1 * 60 * 1000; 
 const VALID_DOMAINS = /(dana\.id|gopay\.co\.id|shopeepay\.co\.id)/i;
